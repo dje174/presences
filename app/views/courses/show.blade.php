8ee0382d14f870c9cf57455dcf040f35227e89cc
dje174 <jeromepoucet@gmail.com>
2013-12-18 08:44:19 +0100

    Modification/suppression cours/student

--- app/views/courses/show.blade.php
+++ app/views/courses/show.blade.php
@@ -3,10 +3,12 @@
 @section('container')
 	<section class="main">
 		<h2>Cours de {{ $course->name }}</h2>
-		{{ link_to_route('courses.edit','Modifier le cours', $course->slug) }}
-		{{ Form::open(array('method' => 'DELETE', 'route' => array('courses.destroy' , $course->slug))) }}
-			{{ Form::submit('Supprimer') }}
-		{{ Form::close() }}
+		<div class="change">
+			{{ link_to_route('courses.edit','Modifier le cours', $course->slug,array('class'=>'modify')) }}
+			{{ Form::open(array('method' => 'DELETE', 'route' => array('courses.destroy' , $course->slug))) }}
+				{{ Form::submit('Supprimer',array('class'=>'delete')) }}
+			{{ Form::close() }}
+		</div>
 		<div class="informationsCourse">
 			<label for="description">Description du cours: <span>{{ $course->description }}</span></label>
 			<label for="level">Cours donné en: <span>{{ $course->level->name }}</span></label>
